baf14cf9792dd56e0aebaa38047c8986933e8f00
JenieX <sozorablack@gmail.com>
2023-09-21 11:29:55 +0300

    fix: add exception to Prettier rule

--- packages/eslint-config/rules/prettier.js
+++ packages/eslint-config/rules/prettier.js
@@ -2,6 +2,7 @@ module.exports = {
   overrides: [
     {
       files: ['*.js'],
+      excludedFiles: ['user.js'],
       extends: ['prettier'],
       rules: {
         '@typescript-eslint/semi': 2,
