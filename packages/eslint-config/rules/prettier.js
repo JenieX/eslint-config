0f8a9af0f7c0ff69d0293047607475cb8c971384
JenieX <sozorablack@gmail.com>
2023-09-23 02:53:31 +0300

    chore: fix prettier exclude rule

--- packages/eslint-config/rules/prettier.js
+++ packages/eslint-config/rules/prettier.js
@@ -2,7 +2,7 @@ module.exports = {
   overrides: [
     {
       files: ['*.js'],
-      excludedFiles: ['user.js'],
+      excludedFiles: ['dummy.js', 'user.js'],
       extends: ['prettier'],
       rules: {
         '@typescript-eslint/semi': 2,
